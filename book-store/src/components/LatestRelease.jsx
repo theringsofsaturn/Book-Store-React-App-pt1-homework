5ebcab9d9425473544c831f043e5f130a51c96af
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-28 17:00:56 +0200

    Latest Release correctly displaying from json data

--- book-store/src/components/LatestRelease.jsx
+++ book-store/src/components/LatestRelease.jsx
@@ -1,19 +1,36 @@
-const BookCategories = ["fantasty", "history", "romance", "scifi", "horror"];
-const books = {
-  fantasty,
-  history,
-  romance,
-  scifi,
-  horror,
-};
+import React from "react";
+import fantasy from "../data/fantasy.json";
+import history from "../data/history.json";
+import horror from "../data/horror.json";
+import romance from "../data/romance.json";
+import scifi from "../data/scifi.json";
 
-class LatestRelease extends React.Component {
-  state = {
-    data: books.fantasty,
-    categorySelected: "fantasy",
-  };
-
-  render() {}
+export default function LatestRelease() {
+  return (
+    <div className="d-flex flex-wrap">
+      {fantasy.map((book) => {
+        return <img className="book-cover" src={book.img} />;
+      })}
+    </div>
+  );
 }
 
-export default LatestRelease;
+// const BookCategories = ["fantasty", "history", "romance", "scifi", "horror"];
+// const books = {
+//   fantasty,
+//   history,
+//   romance,
+//   scifi,
+//   horror,
+// };
+
+// class LatestRelease extends React.Component {
+//   state = {
+//     data: books.fantasty,
+//     categorySelected: "fantasy",
+//   };
+
+//   render() {}
+// }
+
+// export default LatestRelease;
