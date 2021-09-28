175e3cb8a3dbdaee244e20a394d89b1eea005d6b
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-28 16:45:21 +0200

    footer added

---
+++ book-store/src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import Footer from "react-bootstrap/ModalFooter";
+
+const FooterFunc = () => (
+    <footer class='responsiveSiteFooter'>
+    <div class='responsiveSiteFooter__contents gr-container-fluid'>
+    <div class='gr-row'>
+    <div class='gr-col gr-col-md-8 gr-col-lg-6'>
+    <div class='gr-row'>
+    <div class='gr-col-md-3 gr-col-lg-4'>
+    <h3 class='responsiveSiteFooter__heading'>Company</h3>
+    <ul class='responsiveSiteFooter__linkList'>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/about/us">About us</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/jobs">Careers</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/about/terms">Terms</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/about/privacy">Privacy</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="https://help.goodreads.com/s/article/Goodreads-Interest-Based-Ads-Notice">Interest Based Ads</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/adprefs">Ad Preferences</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/help?action_type=help_web_footer">Help</a>
+    </li>
+    </ul>
+    </div>
+    <div class='gr-col-md-4 gr-col-lg-4'>
+    <h3 class='responsiveSiteFooter__heading'>Work with us</h3>
+    <ul class='responsiveSiteFooter__linkList'>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/author/program">Authors</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/advertisers">Advertise</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/news?content_type=author_blogs">Authors &amp; ads blog</a>
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/api">API</a>
+    </li>
+    </ul>
+    </div>
+    <div class='gr-col-md-5 gr-col-lg-4'>
+    <h3 class='responsiveSiteFooter__heading'>Connect</h3>
+    <div class='responsiveSiteFooter__socialLinkWrapper'>
+    <a class="responsiveSiteFooter__socialLink" href="https://www.facebook.com/Goodreads/"><img alt="Goodreads on Facebook" src="https://s.gr-assets.com/assets/site_footer/footer_facebook-ea4ab848f8e86c5f5c98311bc9495a1b.svg" />
+    </a><a class="responsiveSiteFooter__socialLink" href="https://twitter.com/goodreads"><img alt="Goodreads on Twitter" src="https://s.gr-assets.com/assets/site_footer/footer_twitter-126b3ee80481a763f7fccb06ca03053c.svg" />
+    </a></div>
+    <div class='responsiveSiteFooter__socialLinkWrapper'>
+    <a class="responsiveSiteFooter__socialLink" href="https://www.instagram.com/goodreads/"><img alt="Goodreads on Instagram" src="https://s.gr-assets.com/assets/site_footer/footer_instagram-d59e3887020f12bcdb12e6c539579d85.svg" />
+    </a><a class="responsiveSiteFooter__socialLink" href="https://www.linkedin.com/company/goodreads-com/"><img alt="Goodreads on LinkedIn" src="https://s.gr-assets.com/assets/site_footer/footer_linkedin-5b820f4703eff965672594ef4d10e33c.svg" />
+    </a></div>
+    </div>
+    </div>
+    </div>
+    <div class='gr-col gr-col-md-4 gr-col-lg-6 responsiveSiteFooter__appLinksColumn'>
+    <div class='responsiveSiteFooter__appLinksColumnContents'>
+    <div class='responsiveSiteFooter__appLinksColumnBadges'>
+    <a href="https://itunes.apple.com/app/apple-store/id355833469?pt=325668&amp;ct=mw_footer&amp;mt=8"><img alt="Download app for iOS" src="https://s.gr-assets.com/assets/app/badge-ios-desktop-homepage-6ac7ae16eabce57f6c855361656a7540.svg" />
+    </a><a href="https://play.google.com/store/apps/details?id=com.goodreads&amp;utm_source=mw_footer&amp;pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Download app for Android" srcSet="https://s.gr-assets.com/assets/app/badge-android-desktop-home-2x-e31514e1fb4dddecf9293aa526a64cfe.png 2x" src="https://s.gr-assets.com/assets/app/badge-android-desktop-home-0f517cbae4d56c88a128d27a7bea1118.png" />
+    </a></div>
+    <ul class='responsiveSiteFooter__linkList'>
+    <li class='responsiveSiteFooter__linkListItem'>
+    ©
+    2021
+    Goodreads, Inc.
+    </li>
+    <li class='responsiveSiteFooter__linkListItem'>
+    <a class="responsiveSiteFooter__link" href="/toggle_mobile">Mobile version
+    </a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+    </footer>
+);
+
+export default FooterFunc;
