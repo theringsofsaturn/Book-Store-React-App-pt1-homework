3f0264c972d82521a38f57ced5ef37c8be45f933
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-28 16:45:30 +0200

    jumbotron added

---
+++ book-store/src/components/Jumbotron.jsx
@@ -0,0 +1,10 @@
+import Jumbotron from "react-bootstrap/Jumbotron"
+
+const JumbotronFunc = () => (<div class="jumbotron jumbotron-fluid">
+<div class="container">
+  <h1 class="display-4">Gooreads</h1>
+  <p class="lead">Find and read more books you'll love, and keep track of the books you want to read. Be part of the world's largest community of book lovers on Goodreads.</p>
+</div>
+</div>)
+
+export default JumbotronFunc
