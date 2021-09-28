5ea6d23b6307dd3715847e8488b24118966b95cc
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-09-28 16:46:34 +0200

    importing components to app.js

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyNav from "./components/MyNavbar";
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
+import LatestRelease from "./components/LatestRelease"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNav />
+      <Jumbotron />
+      <LatestRelease />
+      <Footer />
     </div>
   );
 }
